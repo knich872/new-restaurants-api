76e32e218ce32dfadba130ba00c642e8857e72d8
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-09-26 12:02:53 +0900

    base controller content added and routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: [ :index ]
+    end
+  end
 end
