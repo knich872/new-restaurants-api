25330026d918a983b6e0918aed6abf25e4c88e3a
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-08 16:32:58 +0900

    added update method to restaurants controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
-  before_action :set_restaurant, only: [ :show ]
+  before_action :set_restaurant, only: [ :show, :update ]
 
   def index
     @restaurants = policy_scope(Restaurant)
@@ -8,10 +8,27 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   def show
   end
 
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
     authorize @restaurant
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.erros.full_messages },
+      status: :unprocessable_entity
   end
 end
